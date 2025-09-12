1f7782c04344ce92c30a2c9fb9a3326dbe2f4d37
Soheil <1994soheil@gmail.com>
2025-09-12 18:19:10 +0330

    Add images and Splash Screen

--- lib/splash_screen.dart
+++ lib/splash_screen.dart
@@ -6,8 +6,30 @@ class SplashScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
+      body: Container(
+        decoration: BoxDecoration(
+        gradient: const LinearGradient(
+        begin: Alignment.topLeft,
+        end: Alignment.bottomRight,
+        colors: [
+        Color(0xFF6DADFC),
+    Color(0xFF0188E9),
+    ],)
 
+        ) ,
+        width: double.infinity,
+
+
+        child: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+          Image.asset('assets/images/blue2.png'),
+          Text("Nike store",style: TextStyle(fontSize: 32,fontWeight: FontWeight.bold,color: Colors.white),),
+          Text("www.nike.com"),
+
+          ],
+        ),
       ),
     );
   }
